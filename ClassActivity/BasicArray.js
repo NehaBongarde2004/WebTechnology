75568d8009f9182cee78d1a814d34886bc5751c4
Neha Bongarde <nehabongarde075@gmail.com>
2026-01-21 21:11:36 +0530

    BasicArray.js added

---
+++ ClassActivity/BasicArray.js
@@ -0,0 +1,24 @@
+//Array declaration
+let myArr = [1,2,3,4,5]
+console.log(`My array values : ${myArr}`)
+console.log(typeof(myArr))
+
+
+const arr = new Array(1,2,3)
+console.log(arr[1])
+
+//array methods
+arr.push(4) //add values in array
+console.log(arr)
+arr.pop()  // remove last value from array
+console.log(arr) 
+arr.unshift(9) //shifts all element and added at 1st position
+console.log(arr)
+console.log(arr.shift()) //
+console.log(arr)
+console.log(arr.includes(7)) //it return true if element present & false if element absent  
+console.log(arr.indexOf(2)) //it return index of given element
+
+//activity - slice and split method
+
+
