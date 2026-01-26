9bdd54b548f6a14cfcf8f153d40f690c3daf696f
Neha Bongarde <nehabongarde075@gmail.com>
2026-01-26 23:17:18 +0530

    DateTime.js added

---
+++ ClassActivity/DateTime.js
@@ -0,0 +1,22 @@
+let myDate = new Date()
+console.log(myDate,typeof(myDate))
+console.log(myDate.toString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toDateString())
+
+console.log(myDate.getDate())
+console.log(myDate.getMonth())
+console.log(myDate.getFullYear())
+console.log(myDate.getUTCDate())
+console.log(myDate.getHours())
+console.log(myDate.getDay())
+
+console.log(myDate.toLocaleString)
+
+
+let myTimeSamp = Date.now()
+console.log(myTimeSamp)
+console.log(Date.now())
+console.log(Date.now()/1000)
